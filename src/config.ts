ef2d36121f5dd44f59f54a020b12e0e425573149
Manuel Astudillo <manuel@optimalbits.com>
2016-12-04 15:30:00 +0100

    added promise cancellation support

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,12 @@
-
+/// <reference path="../node_modules/@types/bluebird/index.d.ts" />
 
 namespace Castmill {
+  window['Bluebird'] = Promise;
+
+  Bluebird.config({
+    cancellation: true,
+  });
+
   export interface Config {
     widgetBase: string;
   }
