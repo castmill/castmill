fef38d32ec3ee52a791de995ce4d2ab03ef875ec
Manuel Astudillo <manuel@optimalbits.com>
2023-05-30 14:52:33 +0200

    chore(castmill): add postgresql service to github actions

--- packages/castmill/config/test.exs
+++ packages/castmill/config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :castmill, Castmill.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   database: "castmill_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
