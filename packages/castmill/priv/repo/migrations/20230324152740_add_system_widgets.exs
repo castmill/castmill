aabe5a503067133e149adbaa98ef616a8b2ec8fb
Manuel Astudillo <manuel@optimalbits.com>
2023-05-30 16:33:10 +0200

    chore(castmill): fix warning

--- packages/castmill/priv/repo/migrations/20230324152740_add_system_widgets.exs
+++ packages/castmill/priv/repo/migrations/20230324152740_add_system_widgets.exs
@@ -3,47 +3,49 @@ defmodule Castmill.Repo.Migrations.AddDefaultWidgets do
 
   # Add default, system wide widgets
   def change do
-
-    for attrs <-  [
-      %{name: "image",
-        uri: "widget://image",
-        schema: %{
-          media_id: :string,
-          size: :string
-        }
-      },
-      %{name: "video",
-        uri: "widget://video",
-        schema: %{
-          media_id: :integer,
-          size: :string
-        }
-      },
-      %{name: "text",
-      uri: "widget://text",
-      schema: %{
-        text: :string,
-        css: :object,
-        font: :object, # { url: string, name: string }
-        animation: :object # { from, perspective, chars }
-        }
-      },
-      %{name: "layout",
-      uri: "widget://layout",
-      schema: %{}
-      },
-      %{name: "template",
-      uri: "widget://template",
-      schema: %{
-        name: :string,
-        template_id: :integer,
-        model: :object
-      }
-      }] do
-      widget = %Castmill.Widgets.Widget{ is_system: :true }
+    for attrs <- [
+          %{
+            name: "image",
+            uri: "widget://image",
+            schema: %{
+              media_id: :string,
+              size: :string
+            }
+          },
+          %{
+            name: "video",
+            uri: "widget://video",
+            schema: %{
+              media_id: :integer,
+              size: :string
+            }
+          },
+          %{
+            name: "text",
+            uri: "widget://text",
+            schema: %{
+              text: :string,
+              css: :object,
+              # { url: string, name: string }
+              font: :object,
+              # { from, perspective, chars }
+              animation: :object
+            }
+          },
+          %{name: "layout", uri: "widget://layout", schema: %{}},
+          %{
+            name: "template",
+            uri: "widget://template",
+            schema: %{
+              name: :string,
+              template_id: :integer,
+              model: :object
+            }
+          }
+        ] do
+      %Castmill.Widgets.Widget{is_system: true}
       |> Castmill.Widgets.Widget.changeset(attrs)
       |> Castmill.Repo.insert!()
     end
-
   end
 end
