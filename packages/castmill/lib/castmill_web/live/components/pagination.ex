f92092acba5660b815e7fb5a7cccb635753e854f
Aron Kornhall <aron@kornhall.se>
2023-06-12 23:25:24 +0200

    chore: indentation

--- packages/castmill/lib/castmill_web/live/components/pagination.ex
+++ packages/castmill/lib/castmill_web/live/components/pagination.ex
@@ -28,74 +28,74 @@ defmodule CastmillWeb.Live.Admin.Pagination do
     )
 
     ~H"""
-  <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-    <div>
-      <p class="text-sm text-gray-700">
-        Showing
-        <span :if={@total_pages > 1}>
+    <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+      <div>
+        <p class="text-sm text-gray-700">
+          Showing
+          <span :if={@total_pages > 1}>
+            <span class="font-medium">
+              <%= @from %>
+            </span>
+            to
+            <span class="font-medium">
+              <%= @to %>
+            </span>
+            of
+          </span>
           <span class="font-medium">
-            <%= @from %>
+            <%= @total_items %>
           </span>
-          to
-          <span class="font-medium">
-            <%= @to %>
-          </span>
-          of
-        </span>
-        <span class="font-medium">
-          <%= @total_items %>
-        </span>
-        results
-      </p>
-    </div>
-    <nav :if={@total_pages > 1} class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-      <.link
-        patch={
-          ~p"/admin/#{@selected_link}?#{%{@options | page: @page - 1}}"
-        }
-        class={"relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 dark:text-slate-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 #{if !@has_previous, do: "pointer-events-none opacity-50"}"}
+          results
+        </p>
+      </div>
+      <nav :if={@total_pages > 1} class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+        <.link
+          patch={
+            ~p"/admin/#{@selected_link}?#{%{@options | page: @page - 1}}"
+          }
+          class={"relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 dark:text-slate-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 #{if !@has_previous, do: "pointer-events-none opacity-50"}"}
 
-      >
-        <span class="sr-only">Previous</span>
-        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
-        </svg>
-      </.link>
+        >
+          <span class="sr-only">Previous</span>
+          <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
+          </svg>
+        </.link>
 
-      <%= for pageNo <- @pages_to_show do %> 
-        <%= if pageNo == nil do %>
-          <span
-            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 dark:text-slate-400 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-            ...
-          </span>
-        <% else %>
-          <.link
-            aria-current={if pageNo == @page, do: "page", else: nil}
-            patch={
-              ~p"/admin/#{@selected_link}?#{%{@options | page: pageNo}}"
-            }
-            class={"#{if pageNo == @page,
-              do: "relative z-10 inline-flex items-center bg-blue-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
-              else: "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 dark:text-slate-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"}"}
-            >
-              <%= pageNo %>
-          </.link>
+        <%= for pageNo <- @pages_to_show do %> 
+          <%= if pageNo == nil do %>
+            <span
+              class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 dark:text-slate-400 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
+              ...
+            </span>
+          <% else %>
+            <.link
+              aria-current={if pageNo == @page, do: "page", else: nil}
+              patch={
+                ~p"/admin/#{@selected_link}?#{%{@options | page: pageNo}}"
+              }
+              class={"#{if pageNo == @page,
+                do: "relative z-10 inline-flex items-center bg-blue-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+                else: "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 dark:text-slate-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"}"}
+              >
+                <%= pageNo %>
+            </.link>
+          <% end %>
         <% end %>
-      <% end %>
 
-      <.link
-        patch={
-          ~p"/admin/#{@selected_link}?#{%{@options | page: @page + 1}}"
-        }
-        class={"relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 #{if !@has_next, do: "pointer-events-none opacity-50"}"}
-      >
-        <span class="sr-only">Next</span>
-        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
-        </svg>
-      </.link>
-    </nav>
-  </div>
+        <.link
+          patch={
+            ~p"/admin/#{@selected_link}?#{%{@options | page: @page + 1}}"
+          }
+          class={"relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 #{if !@has_next, do: "pointer-events-none opacity-50"}"}
+        >
+          <span class="sr-only">Next</span>
+          <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
+          </svg>
+        </.link>
+      </nav>
+    </div>
     """
   end
 end
