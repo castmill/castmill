459293564554ccadb5a1f1c645dcf1ba17ff0cba
Aron Kornhall <aron@kornhall.se>
2024-01-29 18:41:01 +0100

    feat(player): add player menu with some dummy entries

--- packages/device/src/App.tsx
+++ packages/device/src/App.tsx
@@ -4,10 +4,12 @@ import { Device } from "./classes/device";
 import logo from "./logo.svg";
 import styles from "./App.module.css";
 import { DeviceComponent } from "./components/device.component";
+import { MenuComponent } from "./components/menu.component";
 
 const App: Component<{ device: Device }> = (props: { device: Device }) => {
   return (
     <div class={styles.App}>
+      <MenuComponent />
       <DeviceComponent device={props.device} />
     </div>
   );
