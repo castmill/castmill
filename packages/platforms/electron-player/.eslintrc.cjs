c0ba8400709e9a4537224fbf83455b7c5e406864
Aron Kornhall <aron@kornhall.se>
2024-02-05 21:30:55 +0100

    feat(electron): add electron-app

---
+++ packages/platforms/electron-player/.eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ['solid'],
+  extends: [
+    'eslint:recommended',
+    'plugin:solid/typescript',
+    '@electron-toolkit/eslint-config-ts/recommended',
+    '@electron-toolkit/eslint-config-prettier'
+  ]
+}
