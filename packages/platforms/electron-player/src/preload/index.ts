5885513cbc24eb12759e48ef2b011ccf9bd99c13
Aron Kornhall <aron@kornhall.se>
2024-04-24 23:24:48 +0200

    feat(electron): use real storage for credentials

--- packages/platforms/electron-player/src/preload/index.ts
+++ packages/platforms/electron-player/src/preload/index.ts
@@ -1,3 +1,5 @@
+import os from 'os';
+import si from 'systeminformation';
 import { contextBridge, ipcRenderer } from 'electron';
 import { electronAPI } from '@electron-toolkit/preload';
 import type { ApplicationAPI } from './index.d';
@@ -11,7 +13,21 @@ const api: ApplicationAPI = {
   reboot: () => ipcRenderer.send(Action.REBOOT),
   update: () => ipcRenderer.send(Action.UPDATE),
   getMachineGUID: () => ipcRenderer.invoke(Action.GET_MACHINE_GUID),
+  getItem: (key: string) => ipcRenderer.invoke(Action.GET_STORE_VALUE, key),
+  setItem: (key: string, value: string) => ipcRenderer.send(Action.SET_STORE_VALUE, key, value),
+  deleteItem: (key: string) => ipcRenderer.send(Action.DELETE_STORE_VALUE, key),
 };
+
+async function getHardwareInfo() {
+    try {
+        const data = await si.system();
+        console.log(data);  // Logs the complete system data for debugging
+        return data.model;  // Returns the model name, e.g., "MacBookPro15,1" or a specific Windows/Linux machine model
+    } catch (error) {
+        console.error('Error fetching system information:', error);
+        return 'Could not be determined';
+    }
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
@@ -20,6 +36,12 @@ if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI);
     contextBridge.exposeInMainWorld('api', api);
+    contextBridge.exposeInMainWorld('osInfo', {
+        type: os.type(),
+        platform: os.platform(),
+        release: os.release(),
+    });
+    contextBridge.exposeInMainWorld('hardwareInfo', getHardwareInfo);
   } catch (error) {
     console.error(error);
   }
