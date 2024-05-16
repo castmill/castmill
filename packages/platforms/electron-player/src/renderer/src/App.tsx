ef0fedac43dcdfa3684786ac691bb7db6b9a31c4
Aron Kornhall <aron@kornhall.se>
2024-05-16 22:57:34 +0200

    remove version component

--- packages/platforms/electron-player/src/renderer/src/App.tsx
+++ packages/platforms/electron-player/src/renderer/src/App.tsx
@@ -1,12 +1,10 @@
 import type { Component } from 'solid-js';
-import Versions from './components/Versions';
 import PlayerFrame from './components/PlayerFrame';
 
 const App: Component = () => {
   return (
     <>
       <PlayerFrame />
-      <Versions />
     </>
   );
 };
