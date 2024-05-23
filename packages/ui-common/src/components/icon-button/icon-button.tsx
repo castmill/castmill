2fc193d90d3c09cd44c272ffe7d34a750e457d3e
Manuel Astudillo <manuel@optimalbits.com>
2024-05-23 10:17:37 +0200

    refactor(ui-common): move icon-button component to its own directory

--- packages/ui-common/src/components/icon-button/icon-button.tsx
+++ packages/ui-common/src/components/icon-button/icon-button.tsx
@@ -1,5 +1,6 @@
-import { Component } from 'solid-js';
+import { Component, mergeProps } from 'solid-js';
 import './icon-button.scss'; // Import your custom CSS for styles
+import { IconWrapper } from '../icon-wrapper';
 
 export const IconButton: Component<{
   onClick?: () => void;
@@ -7,20 +8,22 @@ export const IconButton: Component<{
   icon: Component; // This should be a component (icon)
   color?: 'primary' | 'secondary' | 'danger' | 'success' | 'info';
 }> = (props) => {
-  const defaultProps = {
-    disabled: false,
-    color: 'primary', // Default color
-    ...props,
-  };
+  const defaultProps = mergeProps(
+    {
+      disabled: false,
+      color: 'primary', // Default color
+    },
+    props
+  );
 
   return (
     <button
       class={`icon-button icon-button-${defaultProps.color} ${defaultProps.disabled ? 'icon-button-disabled' : ''}`}
-      onClick={defaultProps.disabled ? undefined : defaultProps.onClick}
+      onClick={defaultProps.onClick}
       disabled={defaultProps.disabled}
       title={props.icon.name} // Optional: provide a tooltip (icon name or custom title)
     >
-      <props.icon />
+      <IconWrapper icon={props.icon!} />
     </button>
   );
 };
