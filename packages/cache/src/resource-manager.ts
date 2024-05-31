753806cc3b7bc592353836933f8a2decd3e5f20f
Aron Kornhall <aron@kornhall.se>
2024-05-31 20:32:20 +0200

    fix(cache): fix issue causing electron-player build to fail.
    Probably caused by some mismatch in the ES features. I couldn't find a
    switch for not generating code with await in template string in dynamic
    import so I added an if statement to force the compiler to break this
    up.

--- packages/cache/src/resource-manager.ts
+++ packages/cache/src/resource-manager.ts
@@ -101,8 +101,14 @@ export class ResourceManager {
 
     if (item) {
       const code = await this.fetchCode(item.cachedUrl);
-      const uri = `data:text/javascript,${code};`;
-      return import(/* @vite-ignore */ uri) as Promise<T>;
+      if(code) {
+        // This if statement is needed to avoid an error when vite tries to inline an
+        // await inside an import statement.
+        // Example: import(`data:text/javascript,${await this.fetchCode(item.cachedUrl)};`)
+        // When building the electron-player, this throws an error.
+        const uri = `data:text/javascript,${code};`;
+        return import(/* @vite-ignore */ uri) as Promise<T>;
+      }
     }
 
     return this.cache.set(url, ItemType.Code, 'text/javascript');
