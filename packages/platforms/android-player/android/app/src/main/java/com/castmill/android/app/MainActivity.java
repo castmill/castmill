88a15ee654d607966b6216c4d4c590774d0783fe
Aron Kornhall <aron@kornhall.se>
2024-09-24 22:27:26 +0200

    feat(android): autostart on boot and handle watchdog

--- packages/platforms/android-player/android/app/src/main/java/com/castmill/android/app/MainActivity.java
+++ packages/platforms/android-player/android/app/src/main/java/com/castmill/android/app/MainActivity.java
@@ -1,5 +1,50 @@
 package com.castmill.android.app;
 
-import com.getcapacitor.BridgeActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebChromeClient;
+
+public class MainActivity extends BridgeActivity {
+    private static final String TAG = "CastmillPlayer";
+    private static final String CASTMILL_WATCHDOG_PACKAGE = "com.castmill.android.app.watchdog";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(CastmillPlugin.class);
+        super.onCreate(savedInstanceState);
+
+        launchWatchdog();
+        WatchdogHelper.enableWatchdog(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // disables the ugly default video tag poster image
+        this.getBridge().getWebView().setWebChromeClient(new BridgeWebChromeClient(bridge) {
+            @Override
+            public Bitmap getDefaultVideoPoster() {
+                return Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+            }
+        });
+    }
+
+    private void launchWatchdog() {
+        try {
+            Intent launchIntent = this.getPackageManager().getLaunchIntentForPackage(CASTMILL_WATCHDOG_PACKAGE);
+            if (launchIntent == null) {
+                Log.d(TAG, "Couldn't create launch intent for Castmill Watchdog");
+            } else {
+                launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                this.startActivity(launchIntent);
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "Exception when trying to start Watchdog" + e.toString());
+        }
+    }
+}
