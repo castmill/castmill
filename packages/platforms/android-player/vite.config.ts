31b7b31128ac3492d3f296e47fc0dba67c121a51
Aron Kornhall <aron@kornhall.se>
2024-09-24 22:19:32 +0200

    chore(android): add build, test, and prettier

--- packages/platforms/android-player/vite.config.ts
+++ packages/platforms/android-player/vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  plugins: [
+    /* 
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    // devtools(),
+    solidPlugin(),
+  ],
   root: './src',
   build: {
     outDir: '../dist',
