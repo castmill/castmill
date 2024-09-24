0827bca8df754cba002f4e367b7c39834fe1a886
Aron Kornhall <aron@kornhall.se>
2024-09-24 22:23:58 +0200

    feat(android): create plugin for native functionality

---
+++ packages/platforms/android-player/src/plugins/castmill/definitions.ts
@@ -0,0 +1,18 @@
+import type { PluginListenerHandle } from '@capacitor/core';
+
+export interface CastmillPlugin {
+  /**
+   * Restart the Castmill application.
+   */
+  restart(): Promise<void>;
+
+  /**
+   * Reboot the device.
+   */
+  reboot(): Promise<void>;
+
+  /**
+   * Quit the Castmill application.
+   */
+  quit(): Promise<void>;
+}
