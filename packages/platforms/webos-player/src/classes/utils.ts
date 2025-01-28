e24f049f48ec1229a3f797a9b58dd557336d8d56
Aron Kornhall <aron@kornhall.se>
2025-01-28 20:55:49 +0100

    chore(webos): remove unused hash function

--- packages/platforms/webos-player/src/classes/utils.ts
+++ packages/platforms/webos-player/src/classes/utils.ts
@@ -1,18 +1,4 @@
 /**
- * Digests a string into a SHA-256 hash.
- */
-export async function digestText(message: string) {
-  const msgUint8 = new TextEncoder().encode(message);
-  const hashBuffer = await window.crypto.subtle.digest('SHA-256', msgUint8);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  // Convert bytes to hex string
-  const hashHex = hashArray
-    .map((b) => b.toString(16).padStart(2, '0'))
-    .join('');
-  return hashHex;
-}
-
-/**
  * Simple hash function based on the DJB2 algorithm.
  * Returns a hexadecimal string.
  */
