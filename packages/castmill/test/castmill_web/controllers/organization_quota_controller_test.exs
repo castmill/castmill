076b9abf5c242bbcc5d6c5a2a011fccaaafa02da
Manuel Astudillo <manuel@optimalbits.com>
2025-02-01 12:24:37 +0100

    chore: fix formatting

--- packages/castmill/test/castmill_web/controllers/organization_quota_controller_test.exs
+++ packages/castmill/test/castmill_web/controllers/organization_quota_controller_test.exs
@@ -4,7 +4,6 @@ defmodule CastmillWeb.OrganizationQuotaControllerTest do
   import Castmill.QuotasFixtures
   alias CastmillWeb.Router.Helpers, as: Routes
 
-
   @organization_id "00000000-0000-0000-0000-000000000000"
 
   @moduletag :skip
