570302fb461a3c435acf1e8973fe88026d353614
Aron Kornhall <aron@kornhall.se>
2025-02-09 20:33:22 +0100

    fix(platforms): add logging of errors thrown during geolocation

--- packages/platforms/legacy-adapter-player/src/classes/android-legacy-machine.ts
+++ packages/platforms/legacy-adapter-player/src/classes/android-legacy-machine.ts
@@ -100,7 +100,8 @@ export class AndroidLegacyMachine implements LegacyMachine {
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
       };
-    } catch {
+    } catch (error) {
+      console.error(`Failed to get location: ${error}`);
       return undefined;
     }
   }
