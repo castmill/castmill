8a96b137d69880719dad1c419c1159c18038c7e5
Manuel Astudillo <manuel@optimalbits.com>
2025-09-30 10:16:36 +0200

    fix(device): properly format some files

--- packages/device/src/components/pincode.component.tsx
+++ packages/device/src/components/pincode.component.tsx
@@ -4,10 +4,12 @@ export function PincodeComponent(props: { pincode: string }) {
   return (
     <div class={styles.pincodeSection}>
       <div class={styles.pincodeLabel}>Your Registration Code</div>
-      <div class={styles.pincode}>{props.pincode ? props.pincode : 'Loading pincode...'}</div>
+      <div class={styles.pincode}>
+        {props.pincode ? props.pincode : 'Loading pincode...'}
+      </div>
       <div class={styles.instructions}>
-        Enter this code on the Castmill dashboard to register your device,
-        or scan the QR code below with your mobile device.
+        Enter this code on the Castmill dashboard to register your device, or
+        scan the QR code below with your mobile device.
       </div>
     </div>
   );
